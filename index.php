63afea7eb848eff7ac96f84a2f22387c1d48325c
bucreevnikolai2016 <106608745+bucreevnikolai2016@users.noreply.github.com>
2022-05-31 15:15:25 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-ee
-de
+<html>  
+<body>
+
+<form action="action.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
