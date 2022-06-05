72efa6720e1c09769cbc74f7ba839ebd9d9b91f8
bucreevnikolai2016 <106608745+bucreevnikolai2016@users.noreply.github.com>
2022-06-05 12:31:01 +0300

    Update form-submission-handler.js

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -96,10 +96,5 @@
   };
   document.addEventListener("DOMContentLoaded", loaded, false);
 
-  function disableAllButtons(form) {
-    var buttons = form.querySelectorAll("button");
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
-    }
-  }
+  
 })();
